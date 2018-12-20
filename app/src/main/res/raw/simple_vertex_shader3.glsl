26c3a13246e2eb8f0a0839ec68b4605b98374cbe
suikajy <2058550226@qq.com>
2018-12-20 15:40:08 +0800

    ch5 matrix

--- app/src/main/res/raw/simple_vertex_shader3.glsl
+++ app/src/main/res/raw/simple_vertex_shader3.glsl
@@ -1,8 +1,12 @@
-precision mediump float;
+uniform mat4 u_Matrix;
+
+attribute vec4 a_Position;
+attribute vec4 a_Color;
 
 varying vec4 v_Color;
 
-void main()
-{
-    gl_FragColor = v_Color;
+void main() {
+    v_Color = a_Color;
+    gl_Position = u_Matrix * a_Position;
+    gl_PointSize = 10.0;
 }
